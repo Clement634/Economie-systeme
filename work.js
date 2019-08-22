d327c780db8ec7502f1aff1f34c3fe3f74841f99
Jérémy <41241482+slayerMonkeys@users.noreply.github.com>
2019-08-22 02:28:51 +0200

    fix bug

--- work.js
+++ work.js
@@ -10,35 +10,30 @@ class work extends Command {
       category: "Economie"
     });
   }
-
+  if (args[0] === work[1] || work[2] || work[5] || work[8] || work[9]) {
+    let salaire = 1;
+    message.channel.send(`Votre travaille vous à raporté **${salaire}**$`);
+    bot.money.add(`money_${user.id}`, salaire);
+  } else if (args[0] === work[0] || work[4]) {
+    let salaire2 = 2;
+      message.channel.send(`Votre travaille vous à raporté **${salaire2}**$`);
+      bot.money.add(`money_${user.id}`, salaire2);
+  } else if (args[0] === work[3] || work[6] || work[7]) {
+    let salaire3 = 3;
+    message.channel.send(`Votre travaille vous à raporté **${salaire3}**$`);
+    bot.money.add(`money_${user.id}`, salaire3);
+  } else if (args[0] === "mendiant") {
+    let salaire4 = 4;
+    message.channel.send(`Vous mendiez et cela vous rapporte **${salaire4}**$`);
+    bot.money.add(`money_${user.id}`, salaire4);
+  } else {
+    message.channel.send("Vous devez entré un métier valide, pour savoir nos métiers efféctué la commande test.workliste.");
+  }
   async run(message, args) {
     try {
       let user = message.author;
       let work = ["développeur", "barman", "boucher", "pilote", "graphiste", "mineur", "policier", "pompier", "abatteur", "vigneron"];
-      switch (args[0]) {
-        case work[1] || work[2] || work[5] || work[8] || work[9]:
-          let salaire = Math.floor(Math.random() * 100) + 1;
-          message.channel.send(`Votre travaille vous à raporté **${salaire}**$`);
-          db.add(`money_${user.id}`, salaire);
-          break;
-        case work[0] || work[4]:
-          salaire2 = Math.floor(Math.random() * 100) + 100;
-          message.channel.send(`Votre travaille vous à raporté **${salaire2}**$`);
-          db.add(`money_${user.id}`, salaire2);
-          break;
-        case work[3] || work[6] || work[7]:
-            let salaire3 = Math.floor(Math.random() * 200) + 100;
-            message.channel.send(`Votre travaille vous à raporté **${salaire3}**$`);
-            db.add(`money_${user.id}`, salaire3);
-            break;
-        case "prostitué":
-            let salaire4 = Math.floor(Math.random() * 100) + 1;
-            message.channel.send(`Vous faite le tapin et cela vous rapporte **${salaire4}**$`);
-            db.add(`money_${user.id}`, salaire4);
-            break;
-        default: message.channel.send("Vous devez entré un métier valide, pour savoir nos métiers efféctué la commande test.workliste.")
-          break;
-      }
+      
     } catch (e) {
       console.log(e);
     }
